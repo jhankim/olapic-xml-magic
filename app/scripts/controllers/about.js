be7ebe66163af20d2295ef6471203865473f0b83
Jae <jae@olapic.com>
2014-09-05 19:12:01 -0400

    yo angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name feedvalidApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the feedvalidApp
+ */
+angular.module('feedvalidApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
