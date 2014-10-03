5ea2ba967c76ba1bf6304983f26b796557b49b88
Jae <jae@olapic.com>
2014-10-03 18:56:15 -0400

    some updates

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,13 +8,7 @@
  * Controller of the olapicFeedVisualApp
  */
 angular.module('olapicFeedVisualApp')
-  .controller('MainCtrl', function ($scope, $upload) {
-    
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', ['$scope', '$http' ,'$upload', function($scope, $http, $upload){
 
     $scope.oneAtATime = true;
 
@@ -23,40 +17,53 @@ angular.module('olapicFeedVisualApp')
       isFirstDisabled: false
     };
 
-  });
+    $scope.formData = {};
 
+    $scope.urlSubmit = function() {
+      console.log(this.feedUrl);
 
-var uploadCtrl = [ '$scope', '$upload', function($scope, $upload) {
-  $scope.onFileSelect = function($files) {
-    //$files: an array of files selected, each file has name, size, and type.
-    for (var i = 0; i < $files.length; i++) {
-      var file = $files[i];
-      $scope.upload = $upload.upload({
-        url: 'http://localgit:8888/localdev/feedvalid/validate2.php', //upload.php script, node.js route, or servlet url
-        //method: 'POST' or 'PUT',
-        //headers: {'header-key': 'header-value'},
-        //withCredentials: true,
-        data: {myObj: $scope.myModelObj},
-        file: file, // or list of files ($files) for html5 only
-        //fileName: 'doc.jpg' or ['1.jpg', '2.jpg', ...] // to modify the name of the file(s)
-        // customize file formData name ('Content-Disposition'), server side file variable name. 
-        //fileFormDataName: myFile, //or a list of names for multiple files (html5). Default is 'file' 
-        // customize how data is added to formData. See #40#issuecomment-28612000 for sample code
-        //formDataAppender: function(formData, key, val){}
-      }).progress(function(evt) {
-        console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
-      }).success(function(data, status, headers, config) {
-        // file is uploaded successfully
-        console.log(data);
+      var requestUrl = 'http://localgit:8888/localdev/feedvalid/validate2.php?url=' + encodeURIComponent(this.feedUrl);
+
+      $http.get(requestUrl).
+      success(function(data, status, headers, config) {
+        $scope.response = data;
+      }).
+      error(function(data, status, headers, config) {
+        // log error
       });
-      //.error(...)
-      //.then(success, error, progress); 
-      // access or attach event listeners to the underlying XMLHttpRequest.
-      //.xhr(function(xhr){xhr.upload.addEventListener(...)})
     }
-    /* alternative way of uploading, send the file binary with the file's content-type.
-       Could be used to upload files to CouchDB, imgur, etc... html5 FileReader is needed. 
-       It could also be used to monitor the progress of a normal http post/put request with large data*/
-    // $scope.upload = $upload.http({...})  see 88#issuecomment-31366487 for sample code.
-  };
-}];
+
+    $scope.onFileSelect = function($files) {
+      //$files: an array of files selected, each file has name, size, and type.
+      for (var i = 0; i < $files.length; i++) {
+        var file = $files[i];
+        $scope.upload = $upload.upload({
+          url: 'http://localgit:8888/localdev/feedvalid/validate2.php', //upload.php script, node.js route, or servlet url
+          method: 'POST',
+          //headers: {'header-key': 'header-value'},
+          //withCredentials: true,
+          data: {myObj: $scope.myModelObj},
+          file: file, // or list of files ($files) for html5 only
+          //fileName: 'doc.jpg' or ['1.jpg', '2.jpg', ...] // to modify the name of the file(s)
+          // customize file formData name ('Content-Disposition'), server side file variable name. 
+          //fileFormDataName: myFile, //or a list of names for multiple files (html5). Default is 'file' 
+          // customize how data is added to formData. See #40#issuecomment-28612000 for sample code
+          //formDataAppender: function(formData, key, val){}
+        }).progress(function(evt) {
+          console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
+        }).success(function(data, status, headers, config) {
+          // file is uploaded successfully
+          console.log(data);
+        });
+        //.error(...)
+        //.then(success, error, progress); 
+        // access or attach event listeners to the underlying XMLHttpRequest.
+        //.xhr(function(xhr){xhr.upload.addEventListener(...)})
+      }
+      /* alternative way of uploading, send the file binary with the file's content-type.
+         Could be used to upload files to CouchDB, imgur, etc... html5 FileReader is needed. 
+         It could also be used to monitor the progress of a normal http post/put request with large data*/
+      // $scope.upload = $upload.http({...})  see 88#issuecomment-31366487 for sample code.
+    };
+
+  }]);
